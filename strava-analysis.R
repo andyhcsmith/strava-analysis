f15433f0d6f213a7f88c2463f699fbbb4b548743
Andy <andrewhcsmith@gmail.com>
2020-08-16 15:40:35 +1200

    Worked on rmarkdown

--- strava-analysis.R
+++ strava-analysis.R
@@ -12,21 +12,22 @@ library(devtools)
 library(strava)
 library(rStrava)
 library(googlePolylines)
+library(ggmap)
 
 setwd('C:/Users/sma8lw/OneDrive - BP/Documents/Andrew/Other/strava-app')
 
-# Setting up credentials, authorization and tokens
-credentials = read_yaml('credentials.yaml')
-
-app <- oauth_app("strava", credentials$client_id, credentials$secret)
-endpoint <- oauth_endpoint(
-  request = NULL,
-  authorize = "https://www.strava.com/oauth/authorize",
-  access = "https://www.strava.com/oauth/token"
-)
-
-token <- oauth2.0_token(endpoint, app, as_header = FALSE,
-                        scope = "activity:read_all")
+# # Setting up credentials, authorization and tokens
+# credentials = read_yaml('credentials.yaml')
+# 
+# app <- oauth_app("strava", credentials$client_id, credentials$secret)
+# endpoint <- oauth_endpoint(
+#   request = NULL,
+#   authorize = "https://www.strava.com/oauth/authorize",
+#   access = "https://www.strava.com/oauth/token"
+# )
+# 
+# token <- oauth2.0_token(endpoint, app, as_header = FALSE,
+#                         scope = "activity:read_all")
 
 
 # Reading all gpx files
@@ -40,6 +41,19 @@ length(unique(data$id))
 p1 <- plot_facets(data)
 ggsave("plots/facets001.png", p1, width = 20, height = 20, units = "cm")
   
+
+# Plotting routes onto an actual map
+
+mapgilbert <- get_map(location = c(lon = mean(data$lon), lat = mean(data$lat)), zoom = 4,
+                      maptype = "satellite", scale = 2)
+
+ggmap(mapgilbert) +
+  geom_point(data = data, aes(x = lon, y = lat, fill = "red", alpha = 0.8), size = 5, shape = 21) +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE)
+
+data %>% ggplot(aes(lon, lat, group = id)) + 
+  geom_path(data = data, alpha = 0.3,size = 0.3, lineend = "round") + 
+  coord_map(xlim = c(lon_min,lon_max), ylim = c(lat_min, lat_max))
 
 p2 <- plot_map(data, lon_min = 144.75, lon_max = 145.16, lat_min = -38, lat_max = -37.7)
 ggsave("plots/map001.png", p2, width = 20, height = 15, units = "cm", dpi = 600)
